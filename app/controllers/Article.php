718ef6f549951fd99ed6704a87741026c98d113e
surperorange <mail@gongcishi.com>
2019-05-10 16:44:47 +0800

    新增页面底部信息

--- app/controllers/Article.php
+++ app/controllers/Article.php
@@ -2,22 +2,20 @@
 namespace app\controllers;
 
 use thinkview\base\Controller;
-
 use yunyinqing\datacenter\ArticleModule;
 use yunyinqing\datacenter\ObjectModule;
 
-class article extends Controller
+class Article extends Controller
 {
-    protected $data = [
-        'title'=>'',
-        'description'=>''
-    ];
-
     // 首页方法，测试框架自定义DB查询
     public function index($id = 0)
     {
         $siteTitle = ObjectModule::getObject('博客名称');
+        $siteBottom = ObjectModule::getObject('博客底部');
+
         $article = ArticleModule::getArticle($id);
+
+        $this->data['bottom'] = $siteBottom['data']['content'];
 
         $this->data['article'] = $article['data'];
         $this->data['title'] = $article['data']['title'].' - '.$siteTitle['data']['content'];
